dacbbb1cfb08d8a6c3f2a171adf79c22963b2de2
nursultankazbekov <refresh1101>
2021-02-26 10:43:58 +0600

    init commit

---
+++ src/main/java/org/example/grpc/App.java
@@ -0,0 +1,25 @@
+package org.example.grpc;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(8081)
+                .addService(new GreetingServiceImpl())
+                .build();
+
+        server.start();
+
+        System.out.println("server started ...");
+
+        server.awaitTermination();
+    }
+}
